79aecf68a253e54f9e0a86a75096432a4ac92d98
Cristhian Filigrana <adesarrollo1@unico.com.co>
2023-05-25 09:06:49 -0500

    Contador25052023

---
+++ src/components/contador.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="row">
+      <div class="col-2">
+        <button class="btn btn-primary" @click="decrement">-</button>
+      </div>
+      <div class="col-8">
+        <h2>{{ count }}</h2>
+      </div>
+      <div class="col-2">
+        <button class="btn btn-primary" @click="increment">+</button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'contadorVue',
+    data() {
+      return {
+        count: 0
+      }
+    },
+    methods: {
+      increment() {
+        this.count++;
+      },
+      decrement() {
+        if (this.count > 0) {
+          this.count--;
+        }
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  /* Estilos CSS específicos del componente */
+  </style>
+  
